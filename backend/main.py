3f32c3dfff804b86807ab8588096a033f1ca6d14
LeVanTai19 <levantai2106tv@gmail.com>
2026-06-15 23:29:40 +0700

    WIP: update function commit_bid and reveal_bid, wrap it into transaction

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
 from routers.auction import router as auction_router
 from routers.auth import router as auth_router
 
-from database import engine, Base
-Base.metadata.create_all(bind=engine) # lệnh giúp tự tạo file auction.db
+from database import engine, Base, SessionLocal
+from models import User, Auction
+
+Base.metadata.create_all(bind=engine) # lệnh giúp tự tạo file auction.db và tạo bảng
 
 app = FastAPI(title="Decentralized Auction API")
 
@@ -16,6 +19,32 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# ===== Tạo dữ liệu mồi lúc khởi động =====
+def seed_data():
+    db = SessionLocal()
+
+    # Tạo gói thầu mặc định
+    if not db.query(Auction).first():
+        default_auction = Auction(title = "Dự án xây cầu Alpha", phase = "COMMIT")
+        db.add(default_auction)
+
+    # Tạo các User mặc định
+    users_to_seed = [
+        {"id": "admin", "role": "admin", "balance": 0},
+        {"id": "user1", "role": "user", "balance": 10000},
+        {"id": "user2", "role": "user", "balance": 10000}
+    ]
+
+    for u in users_to_seed:
+        if not db.query(User).first():
+            new_user = User(id = u["id"], role = u["role"], balance = u["balance"])
+            db.add(new_user)
+
+    db.commit()
+    db.close()     
+
+seed_data()   
 
 app.include_router(auction_router, prefix="/api/auction")
 
--- backend/services/contract.py
+++ backend/services/contract.py
@@ -1,52 +1,83 @@
 import hashlib
+from sqlalchemy.orm import Session 
+from models import User, Auction, Bid
 
 class ReverseAuctionContract:
-    def __init__(self):
-
-        #khai báo biến giai đoạn
-        self.phase = "COMMIT"
-
-        #Khai báo biến lưu
-        self.commitments = {} #lưu id và hash
-        self.valid_bid = {} #lưu id và giá thực
 
     # -------- PHASE 1: method nộp thầu --------
-    def commit_bid (self, user_id: str, hash_value: str):
-        if self.phase != "COMMIT":
-            return {"error": "Bạn đang không trong thời gian nộp thầu!"}
+    def commit_bid (self, db: Session, user_id: str, hash_value: str, auction_id: int = 1) -> dict:
+        try:
+            auction = db.query(Auction).filter(Auction.id == auction_id).first()
+            if not auction or auction.phase != "COMMMIT":
+                return {"error": "Lỗi: Không trong giai đoạn nộp thầu!"}
+            
+            user = db.query(User).filter(User.id == user_id).first()
+
+            existing_bid = db.query(Bid).filter(Bid.user_id == user_id, Bid.auction_id == auction_id).first()
+            if existing_bid:
+                return {"error": "Lỗi: Bạn đã nộp thầu dự án này rồi!"}
+            
+            # Logic Đặt cọc:
+            STAKE_AMOUNT = 500.0
+            if user.balance < STAKE_AMOUNT:
+                return {"error": f"Lỗi: Số dư của bạn không đủ! Cần {STAKE_AMOUNT}$ để đặt cọc"}
+            
+            # 1. Trừ tiền cọc
+            user.balance -= STAKE_AMOUNT
+
+            # 2. Tạo hồ sơ Bid mới
+            new_bid = Bid (
+                user_id = user_id,
+                auction_id = auction_id,
+                hash_value = hash_value,
+                is_staked = True
+            )
+            db.add(new_bid)
+
+            db.commit()
+            return {"success": f"Nộp Hash thành công! Hệ thống đã tạm giữ {STAKE_AMOUNT}$ tiền cọc"}
+
+        except Exception as e:
+            db.rollback()
+            return {"error": f"Lỗi hệ thống: {e}"}
         
-        if user_id in self.commitments:
-            return {"error": "Bạn đã nộp thầu trước đó rồi!"}
-        
-        self.commitments[user_id] = hash_value
-        return {"success": f"Đã ghi nhận hash của {user_id}"}
-    
     # -------- PHASE 2: method công bố giá thầu --------
-    def reveal_bid (self, user_id: str, real_price: int, secret_salt: str ):
-        if self.phase != "REVEAL":
-            return {"error": "Bạn đang không trong thời gian tiết lộ!"}
-        
-        if user_id not in self.commitments:
-            return {"error": "Bạn chưa nộp thầu trước đó!"}
-        
-        if user_id in self.valid_bid:
-            return {"error": "Bạn đã tiết lộ giá thầu rồi!"}
-        
-        # So sánh hash tự generate với hash được lưu ở phase 1
-        raw_string = f"{real_price}-{secret_salt}"
-        print(f"==== DEBUG: Chuỗi đem đi băm là: {raw_string} ====")
-        print(f"==== DEBUG: Hash trong hệ thống đang lưu là {self.commitments[user_id]} ====")
+    def reveal_bid (self, db: Session, user_id: str, actual_price: float, secret_salt: str, auction_id: int = 1) -> dict:
+        try: 
+            auction = db.query(Auction).filter(Auction.id == auction_id).first()
+            if not auction or auction.phase != "REVEAL":
+                return {"error": "Lỗi: Chưa đến thời gian công bố giá thầu!"}
+            
+            bid = db.query(Bid).filter(Bid.user_id == user_id, Bid.auction_id == auction_id).first()
+            if not bid:
+                return {"error": "Lỗi: Bạn chưa nộp hồ sơ ẩn ở giai đoạn COMMIT!"}
+            
+            if bid.real_price is not None:
+                return {"error": "Lỗi: Bạn đã công bố giá thành công rồi!"}
+            
+            # Tái tạo và đối chiếu mã hash
+            raw_string = f"{actual_price}-{secret_salt}"
+            generated_hash = hashlib.sha256(raw_string.encode('utf-8')).hexdigest()
 
-        hash_phase2 = hashlib.sha256(raw_string.encode('utf-8')).hexdigest()
-        print(f"==== DEBUG: Hash vừa tự tạo ra là: {hash_phase2} ====")
+            if generated_hash == bid.hash_value:
+                # 1. Lưu giá thật vào DB
+                bid.real_price = actual_price
 
-        hash_phase1 = self.commitments[user_id]
+                # 2. Hoàn trả cọc 
+                user = db.query(User).filter(User.id == user_id).first()
+                user.balance += 500.0
+                bid.is_staked = False
 
-        if hash_phase2 == hash_phase1:
-            self.valid_bid[user_id] = real_price
-            return {"success": f"Đã xác thực thành công! Giá thực của bạn là {real_price}"}
-        else:
-            return {"failed": f"Xác thực thất bại: Giá hoặc mã bí mật sai/gian lận"}
+                db.commit()
+                return {"success": f"Xác thực thành công! Đã hoàn trả 500$ cọc cho {user_id}."}
+            
+            else:
+                return {"error": "Xác thực THẤT BẠI: Giá thực tế hoặc mã bí mật bị sai!"}
+
+        except Exception as e:
+            db.rollback()
+            return {"error": f"Lỗi hệ thống: {e}"}
+
         
     # -------- PHASE 3: Công bố người có giá thấp nhất --------
     def get_winner(self) -> dict:
